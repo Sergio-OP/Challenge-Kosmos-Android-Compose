3a60e9b6020aff3d6b457e48b6915b11936c9dc7
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2024-02-26 13:32:56 -0600

    character entity added

--- app/src/main/java/com/example/challengekosmosandroidcompose/domain/entities/Character.kt
+++ app/src/main/java/com/example/challengekosmosandroidcompose/domain/entities/Character.kt
@@ -1,13 +1,14 @@
 package com.example.challengekosmosandroidcompose.domain.entities
 
 data class Character(
+    val id: Long,
     val name: String,
     val imageSource: String,
-    val status: String,
-    val species: String,
-    val type: String,
-    val gender: String,
-    val origin: String,
-    val location: String,
+    val status: String?,
+    val species: String?,
+    val type: String?,
+    val gender: String?,
+    val origin: String?,
+    val location: String?,
     val details: Boolean = false,
 )
